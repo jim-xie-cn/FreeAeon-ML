5f40074973687d09cbb04104c5ab25b2f0827f85
Jim Xie <jim.xie.cn@outlook.com>
2025-06-11 15:56:00 +0800

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def parse_requirements(filename):
 
 setup(
     name="FreeAeon-ML",
-    version="0.1.0",
+    version="0.1.1",
     author="Jim Xie",
     author_email="jim.xie.cn@outlook.com",
     description="A comprehensive machine learning toolkit for data analysis, preprocessing, modeling, and evaluation.",
